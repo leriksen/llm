5e6d08c0a361da24f32bce7254bb51e0db74eefc
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-10-19 21:25:54 +1100

    tedt document boundaries

--- tests/test_vocabulizerv1.py
+++ tests/test_vocabulizerv1.py
@@ -38,3 +38,17 @@ def test_decode_unknown():
   vocab = Vocabulizerv1(['here', 'they', 'be'])
 
   assert vocab.decode([2, 1, 4]) == "be they <|unk|>"
+
+def test_encode_doc_boundaries():
+  text1 = "be they"
+  text2 = "aware"
+  text = " <|endoftext|> ".join((text1, text2))
+
+  vocab = Vocabulizerv1(['here', 'they', 'be'])
+
+  assert vocab.encode(text) == [2, 1, 3, 4]
+
+def test_decode_doc_boundaries():
+  vocab = Vocabulizerv1(['here', 'they', 'be'])
+
+  assert vocab.decode([2, 1, 3, 4]) == "be they <|endoftext|> <|unk|>"
